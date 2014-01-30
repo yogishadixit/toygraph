4e4df5dbf98b75ff5aefdf65893c9885be1c2202
Yogisha Dixit <yad4@cornell.edu>
2014-01-30 10:28:00 -0500

    Changed edges data stucture from list to dictionary

--- toygraph.py
+++ toygraph.py
@@ -1,19 +1,26 @@
 class ToyGraph:
   """A simple implementation of graph manipulation software"""
 
-  	def __init__(self):
-  	  self.nodes = []
-  	  self.edges = []
+  def __init__(self):
+  	self.nodes = []
+  	self.edges = {}
 
-  	def add_edge(a, b):
-  	  if nodes.count(a) == 0:
-  	    nodes.append(a)
-  	  if nodes.count(b) == 0:
-  	    nodes.append(b)
-  	  edges.append(a,b)
+  def add_edge(a, b, weight = 1.0):
+  	if nodes.count(a) == 0:
+  	  nodes.append(a)
+  	  edges[a] = []
+  	if nodes.count(b) == 0:
+  	  nodes.append(b)
+  	  edges[b] = []
+  	edges[a].append(b, weight)
+  	edges[b].append(a, weight)
 
-  	def add_edges(x):
-  	  for edge in x:
-  	    add_edge(edge)
+  def add_edges(x):
+  	for edge in x:
+  	  add_edge(edge)
 
-  	
+  def degree():
+    degrees = []
+    for node in nodes:
+      degrees.append((node, len(edges[node])))
+    return degrees
