590c14737162b91e4869ba8cd042b5064a4afc22
Yogisha Dixit <yad4@cornell.edu>
2014-01-30 10:07:45 -0500

    Updated readme file and begun implementation

---
+++ toygraph.py
@@ -0,0 +1,19 @@
+class ToyGraph:
+  """A simple implementation of graph manipulation software"""
+
+  	def __init__(self):
+  	  self.nodes = []
+  	  self.edges = []
+
+  	def add_edge(a, b):
+  	  if nodes.count(a) == 0:
+  	    nodes.append(a)
+  	  if nodes.count(b) == 0:
+  	    nodes.append(b)
+  	  edges.append(a,b)
+
+  	def add_edges(x):
+  	  for edge in x:
+  	    add_edge(edge)
+
+  	
