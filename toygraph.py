fb74d2d56e1f88b574fb8ea65a027536a88616c1
Yogisha Dixit <yad4@cornell.edu>
2014-03-13 09:53:07 -0400

    Fully debugged. Still need to implement betweenness and connected components

--- toygraph.py
+++ toygraph.py
@@ -1,4 +1,4 @@
-fron collections import deque
+from collections import deque
 
 class ToyGraph:
   """A simple implementation of graph manipulation software"""
@@ -7,99 +7,107 @@ class ToyGraph:
   	self.nodes = []
   	self.edges = {}
 
-  def add_edge(a, b, weight = 1.0):
-  	if nodes.count(a) == 0:
-  	  nodes.append(a)
-  	  edges[a] = []
-  	if nodes.count(b) == 0:
-  	  nodes.append(b)
-  	  edges[b] = []
-  	edges[a].append(b, weight)
-  	edges[b].append(a, weight)
+  def add_edge(self, a, b, weight = 1.0):
+  	if self.nodes.count(a) == 0:
+  	  self.nodes.append(a)
+  	  self.edges[a] = []
+  	if self.nodes.count(b) == 0:
+  	  self.nodes.append(b)
+  	  self.edges[b] = []
+  	self.edges[a].append((b, weight))
+  	self.edges[b].append((a, weight))
 
-  def add_edges(x):
-  	for edge in x:
-  	  add_edge(edge)
+  def add_edges(self, x):
+    for edge in x:
+      if len(edge) == 2:
+        self.add_edge(edge[0], edge[1])
+      else:
+        self.add_edge(edge[0], edge[1], edge[2])
 
-  def degree():
+  def degree(self):
     degrees = []
-    for node in nodes:
-      degrees.append((node, len(edges[node])))
+    for node in self.nodes:
+      degrees.append((node, len(self.edges[node])))
     return degrees
 
-  def BFS(x, y):
+  def BFS(self, x, y):
     Q = deque()
     V = set()
     Q.append(x)
-    V.update(x)
-    while !Q:
+    V.add(x)
+    while (len(Q) != 0):
       t = Q.popleft()
       if t == y:
         return t
-      for u in edges[t]:
+      for u,w in self.edges[t]:
         if u not in V:
-          V.update(u)
+          V.add(u)
           Q.append(u)
     return None
 
-  def DFS(x, y):
-    S = []
+  def DFS(self, x, y):
+    Q = deque()
     V = set()
     Q.append(x)
-    V.update(x)
-    while !Q.empty():
+    V.add(x)
+    while (len(Q) != 0):
       t = Q.pop()
       if t == y:
         return t
-      for u in edges[t]:
+      for u,w in self.edges[t]:
         if u not in V:
-          V.update(u)
+          V.add(u)
           Q.append(u)
     return None
 
   # uses breadth first search
-  def shortest_distances(x):
+  def shortest_distances(self, x):
     D = {}
-    for node in nodes:
+    for node in self.nodes:
       D[node] = -1
     Q = deque()
     Q.append(x)
     D[x] = 0
-    while !Q:
+    while (len(Q) != 0):
       t = Q.popleft()
-      for edge in edges[t]:
+      for edge in self.edges[t]:
         u = edge[0]
-        if D[u] = -1:
+        if (D[u] == -1):
           D[u] = D[t] + edge[1]
           Q.append(u)
     return D
 
-  def shortest_path(x, y):
+  def shortest_path(self, x, y):
     Prev = {}
-    for node in nodes:
+    for node in self.nodes:
       Prev[node] = -1
     Q = deque()
     Q.append(x)
     Prev[x] = x
-    while !Q:
+    while (len(Q) != 0):
       t = Q.popleft()
       if y == t:
         path = []
-        #TODO: print shortest path
+        temp = t
+        while (temp != x):
+          path.append(temp)
+          temp = Prev[temp]
+        path.append(x)
+        path.reverse()
         return path
-      for edge in edges[t]:
+      for edge in self.edges[t]:
         u = edge[0]
-        if Prev[u] = -1:
+        if (Prev[u] == -1):
           Prev[u] = t
           Q.append(u)
     return None
 
 
 
-  def closeness():
+  def closeness(self):
     cl = []
-    for node in nodes:
-      sp = shortest_distances(node)
+    for node in self.nodes:
+      sp = self.shortest_distances(node)
       farness = 0
       for e in sp:
         farness = farness + sp[e]
